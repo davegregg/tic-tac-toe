e156141796f8926e89e3c910d9fc6b95ab440b55
davegregg@gmail.com <davegregg@gmail.com>
2020-06-01 15:30:29 -0400

    starter code ready

---
+++ main.js
@@ -0,0 +1,26 @@
+let currentPlayer = 'X'; // X is the starting player.
+let playerXSelections = [];
+let playerOSelections = [];
+
+const winningCombinations = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+];
+
+// Get all .grid-cell elements from the DOM and store in cellElementArray (see Resources links below):
+const cellElementArray = document.querySelectorAll('.grid-cell');
+
+for (let elementIndex = 0; elementIndex < cellElementArray.length; elementIndex += 1) {
+    const currentCellElement = cellElementArray[elementIndex]
+
+    currentCellElement.addEventListener('click', function (event) {
+        const clickedCellElement = event.target;
+        console.log("You clicked on cell number: " + clickedCellElement.id)
+    });
+}
